7ca6ecca7b88ea1128a9393b712b1e921ecf9a09
Carter-Burr <91906280+Carter-Burr@users.noreply.github.com>
2022-07-18 15:06:07 -0700

    This is adding my first java project

---
+++ Java Projects/gameBuilder.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class gameBuilder {
+   
+   public static void main(String[] args) {
+      
+      System.out.println("This is a game which builds as you play it");
+      
+   }
+
+}
