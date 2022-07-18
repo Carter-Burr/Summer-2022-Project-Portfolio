e3d2e295a899b480b4f2a0f31f206c90ba72e79a
Carter-Burr <91906280+Carter-Burr@users.noreply.github.com>
2022-07-18 15:14:23 -0700

    This is my second commit for java

--- Java Projects/gameBuilder.java
+++ Java Projects/gameBuilder.java
@@ -1,4 +1,12 @@
+/* 
+   This is my first project over the summer, and I decided to make a game that allows
+   users to customize and add to it while they play it to increase the lenght.
+   I decided to do this because I wanted to improve my skills using 
+*/
+
+
 import java.util.*;
+import java.io.*;
 
 public class gameBuilder {
    
@@ -7,5 +15,7 @@ public class gameBuilder {
       System.out.println("This is a game which builds as you play it");
       
    }
+   
+   
 
 }
